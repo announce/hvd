819108a38d482438a8d59a5d9f631d6c835e2c9d
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2017-04-23 00:31:13 +0900

    Updated libs

--- hpcc/boot.bash
+++ hpcc/boot.bash
@@ -7,8 +7,11 @@ cd $PBS_O_WORKDIR
 conda create --yes --prefix $HOME/py27 python=2.7
 source activate $HOME/py27
 
+LOG_DIR="logs"
+[[ -d "${LOG_DIR}" ]] || mkdir ${LOG_DIR}
+
 export _APP_DIR="$HOME/hpcc"
 export DISPLAY=:0
 pip install -r ${_APP_DIR}/requirements.txt \
   && time python ${_APP_DIR}/vcc-combine.py \
-  && mv {figure_*.png,boot.sh.o*} logs/
+  && mv {figure_*.png,boot.sh.o*} ${LOG_DIR}
