cd6fc6ee83b152905be651be1a6b6af2a5e75ab9
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2017-04-25 18:42:17 +0900

    Updated boot script

--- hpcc/boot.bash
+++ hpcc/boot.bash
@@ -7,11 +7,12 @@ cd $PBS_O_WORKDIR
 conda create --yes --prefix $HOME/py27 python=2.7
 source activate $HOME/py27
 
+APP_DIR="$HOME/hpcc"
 LOG_DIR="logs"
+TARGET_DATA="vcc_data.npz"
+
 [[ -d "${LOG_DIR}" ]] || mkdir ${LOG_DIR}
 
-export _APP_DIR="$HOME/hpcc"
-export DISPLAY=:0
-pip install -qr ${_APP_DIR}/requirements.txt \
-  && time python ${_APP_DIR}/vcc-combine.py 'vcc_data.npz' \
+pip install -qr ${APP_DIR}/requirements.txt \
+  && time python ${APP_DIR}/vcc-combine.py ${TARGET_DATA} \
   && sh ${HOME}/bin/clean.sh
